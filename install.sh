2125d449e6b85590e84181e01e256e5f11e4525d
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2025-10-18 14:09:50 +1100

    Update install.sh
    Edit ZIP URL

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## CONFIG
-ZIP_URL="https://github.com/ASlimeInAHoodie/alertbeat/blob/main/venv.tar.gz"
+ZIP_URL="https://github.com/ASlimeInAHoodie/alertbeat/raw/refs/heads/main/venv.tar.gz"
 USERNAME="alertbeat"
 
 ## PRIVILEGES
